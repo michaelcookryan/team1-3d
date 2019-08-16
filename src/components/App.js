a91f31a5042da30b296cf57468aa6c0bc3af0724
Mike Ryan <mryan@SURFACEPRO.leighfs3.local>
2019-08-16 12:36:24 -0700

    Initial architecure

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Switch, Route } from 'react-router-dom';
+import "./styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      
+      {/* <Switch>
+          <Route path="/" exact component={Warehouses} />
+          <Route path="/warehouses" component={Warehouses} />
+        </Switch> */}
+
+    </div>
+  );
+}
+
+export default App;
