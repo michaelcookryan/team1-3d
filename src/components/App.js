b5f475fef0ef9a798acad4d5013862ed859bef16
Mike Ryan <mryan@SURFACEPRO.leighfs3.local>
2019-08-16 12:58:02 -0700

    small updates

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,25 @@ import React from "react";
 import { Switch, Route } from 'react-router-dom';
 import "./styles/App.css";
 
-function App() {
-  return (
-    <div className="App">
-      
-      {/* <Switch>
+class App extends React.Component {
+
+  state = {
+
+  }
+  render(){
+    
+    return (
+      <div className="App">
+
+        {/* <Switch>
           <Route path="/" exact component={Warehouses} />
           <Route path="/warehouses" component={Warehouses} />
         </Switch> */}
 
-    </div>
-  );
+      </div>
+    );
+  }
+  
 }
 
 export default App;
